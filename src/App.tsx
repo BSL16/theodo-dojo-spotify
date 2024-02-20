f6e0ff3d24531ecf980d826c108934f69cde0f70
BSL16 <bambasakho02@gmail.com>
2024-02-20 19:32:05 +0100

    Changement de texte

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,10 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Bienvenue sur le blind test</h1>
+        <h1 className="App-title">Bienvenue sur le blind test fait par Bamba</h1>
       </header>
       <div className="App-images">
-        <p>Il va falloir modifier le code pour faire un vrai blind test !</p>
+        <p>C'est parti!</p>
       </div>
       <div className="App-buttons"></div>
     </div>
